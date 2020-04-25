1bc4dad5a1c14f3f14bf7666c07a256b32345957
Bryce Langille <brycelangille@Bryces-MacBook-Pro.local>
2020-04-25 14:30:06 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,33 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+//let cardOne = "queen";
+//let cardTwo = "queen";
+//let cardThree = "king";
+//let cardFour = "king";
+const cards= ["queen", "queen", "king" , "king"];
+card[0]="";
+cardOne[1]=["queen"];
+cardTwo[2]=["queen"];
+cardThree[3]=["king"];
+cardFour[4]=["king"];
+
+const cardInPlay = []; 
+cardInPlay.push('cardOne');
+cardInPlay.push('cardTwo');
+
+Function flipCard() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0]=== cardsInPlay[1])
+  }
+  alert("your found a match!");
+} else {
+  alert("Sorry,try again.");
+    }
+  }
+}
+
+console.log("User flipped queen");
 console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+console.log("User flipped king");
 console.log("User flipped " + cardThree);
+console.log("User flipped " + cardFour);
