dba4ddecae0e2e27ede7baea1a9b49e6df041b1e
Bryce Langille <brycelangille@Bryces-MacBook-Pro.local>
2020-04-25 14:52:10 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -10,24 +10,28 @@ cardTwo[2]=["queen"];
 cardThree[3]=["king"];
 cardFour[4]=["king"];
 
-const cardInPlay = []; 
-cardInPlay.push('cardOne');
-cardInPlay.push('cardTwo');
+//let cardOne = cards[0];
+//cardsInPlay.push(cardOne);
+//console.log("User flipped " + cardOne);
 
-Function flipCard() {
-  if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0]=== cardsInPlay[1])
-  }
-  alert("your found a match!");
-} else {
-  alert("Sorry,try again.");
-    }
-  }
-}
+//let cardTwo = cards[2];
+//cardsInPlay.push(cardTwo);
+//console.log("User flipped " + cardTwo);
 
+let cardsInPlay = [];
 console.log("User flipped queen");
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
 console.log("User flipped king");
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardFour);
+
+Function checkForMatch() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+    } else {
+      console.log("Sorry, try again.");
+    }
+
+function flipCard(){
+  const cardId();
+  console.log("User flipped" + cards[cardId]);
+  flipCard.push(cardsInPlay);
+}
